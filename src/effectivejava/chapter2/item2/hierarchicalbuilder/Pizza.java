074627e53fc7327965c5d723a418f55aa9a10544
kuanghc <609779612@qq.com>
2023-09-09 20:27:35 +0800

    add builder class

--- src/effectivejava/chapter2/item2/hierarchicalbuilder/Pizza.java
+++ src/effectivejava/chapter2/item2/hierarchicalbuilder/Pizza.java
@@ -4,6 +4,7 @@ import java.util.EnumSet;
 import java.util.Objects;
 import java.util.Set;
 
+// TODO: 2023/9/9 Best practice: use of the self-type idiom for fluent hierarchies
 // Builder pattern for class hierarchies (Page 14)
 
 // Note that the underlying "simulated self-type" idiom  allows for arbitrary fluid hierarchies, not just builders
@@ -17,7 +18,7 @@ public abstract class Pizza {
   Pizza(Builder<?> builder) {
     toppings = builder.toppings.clone(); // See Item 50
   }
-  
+
   abstract static class Builder<T extends Builder<T>> {
 
     EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
